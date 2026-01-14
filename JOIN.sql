f4f11a15255225e8d1295e079b7cc1837797adee
Mouli Aggarwal <aggarwalmouli15@gmail.com>
2026-01-14 11:59:56 +0530

    Update JOIN.sql

--- JOIN.sql
+++ JOIN.sql
@@ -1,1 +1,22 @@
-
+#1.
+SELECT matchid, player
+FROM goal
+WHERE teamid = 'GER';
+------------------------------------------------------------
+#2.
+SELECT id, stadium, team1, team2
+FROM game
+WHERE id = 1012;
+--------------------------------------------------------------
+#3. 
+SELECT player,teamid,stadium,mdate
+FROM game JOIN goal ON (id=matchid)
+AND teamid = 'GER'
+------------------------------------------------------------------
+#4. 
+SELECT team1 , team2 , player
+FROM game
+JOIN goal 
+ON game.id = goal.matchid
+WHERE player LIKE 'Mario%'
+-------------------------------------------------------------------
