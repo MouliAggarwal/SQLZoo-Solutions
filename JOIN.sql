18be63d13271354c95d16d8420656f9d54d9b04d
Mouli Aggarwal <aggarwalmouli15@gmail.com>
2026-01-15 18:17:51 +0530

    Update JOIN.sql

--- JOIN.sql
+++ JOIN.sql
@@ -20,3 +20,38 @@ JOIN goal
 ON game.id = goal.matchid
 WHERE player LIKE 'Mario%'
 -------------------------------------------------------------------
+#5. 
+SELECT player, teamid, coach, gtime
+FROM goal 
+JOIN eteam 
+ON teamid=id
+WHERE gtime<=10 
+---------------------------------------------------------------------
+#6.
+SELECT mdate , teamname
+FROM game
+JOIN eteam
+ON team1=eteam.id 
+WHERE coach LIKE 'Fernando Santos'
+--------------------------------------------------------------------------
+#7.
+SELECT mdate , teamname
+FROM game
+JOIN eteam
+ON team1=eteam.id 
+WHERE coach LIKE 'Fernando Santos'
+----------------------------------------------------------------------------
+#8.
+SELECT DISTINCT player
+FROM game 
+JOIN goal 
+ON matchid = id 
+WHERE teamid!='GER' 
+AND (team1 = 'GER' OR team2 = 'GER')
+------------------------------------------------------------------------------
+#9.
+SELECT teamname, COUNT(teamid)
+FROM eteam 
+JOIN goal ON id=teamid
+GROUP BY teamname
+-------------------------------------------------------------------------------
