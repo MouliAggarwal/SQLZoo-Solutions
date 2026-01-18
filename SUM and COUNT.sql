ca873bb31d1ab17d2dfc5823e3ba3dd42f890170
Mouli Aggarwal <aggarwalmouli15@gmail.com>
2026-01-18 14:11:36 +0530

    Update SUM and COUNT.sql

--- SUM and COUNT.sql
+++ SUM and COUNT.sql
@@ -26,3 +26,15 @@ SELECT continent , COUNT(continent)
 FROM world
 GROUP BY continent
 --------------------------------------------------------------
+#7. Counting big countries in each continent
+SELECT continent , COUNT(name)
+FROM world
+WHERE population >= 10000000
+GROUP BY continent
+----------------------------------------------------------------------
+#8. Counting big continents
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
+---------------------------------------------------------------------
