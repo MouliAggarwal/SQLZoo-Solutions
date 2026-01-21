8b652014a8c781546f914cdac4254cfba762e07e
Mouli Aggarwal <aggarwalmouli15@gmail.com>
2026-01-21 23:45:06 +0530

    Update More JOIN operations.sql

--- More JOIN operations.sql
+++ More JOIN operations.sql
@@ -24,4 +24,37 @@ SELECT id
 FROM movie
 WHERE title = 'Casablanca' AND yr = '1942'
 --------------------------------------------------------
+#6. Cast list for Casablanca
+SELECT name
+FROM actor
+JOIN casting
+ON id = actorid
+WHERE movieid = 132689;
+------------------------------------------------------------
+#7. Alien cast list
+SELECT a.name
+FROM actor a
+JOIN casting c ON a.id = c.actorid
+JOIN movie m ON c.movieid = m.id
+WHERE m.title = 'Alien'
+--------------------------------------------------------------
+#8. Harrison Ford movies
+SELECT m.title
+FROM movie m
+JOIN casting c 
+ON m.id = c.movieid
+JOIN actor a 
+ON c.actorid = a.id
+WHERE a.name = 'Harrison Ford'
+----------------------------------------------------------------------
+#9. Harrison Ford as a supporting actor
+SELECT m.title
+FROM movie m
+JOIN casting c 
+ON m.id = c.movieid
+JOIN actor a 
+ON c.actorid = a.id
+WHERE a.name = 'Harrison Ford'
+AND c.ord <> '1'
+---------------------------------------------------------------------------
 
